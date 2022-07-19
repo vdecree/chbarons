23b4cae6d43b8dcb2dc65bf25e0b36fcbfed9821
vdecree <hello@matdoidge.co.uk>
2022-07-19 19:30:42 +0100

    updated projects page

--- www/site/templates/default.php
+++ www/site/templates/default.php
@@ -20,11 +20,16 @@
 ?>
 <?php snippet('header') ?>
 
-<article>
-  <h1 class="h1"><?= $page->title()->html() ?></h1>
-  <div class="text">
-    <?= $page->text()->kt() ?>
-  </div>
-</article>
+<section>
+  <div class="wrapper">
+    <article>
+      <h1 class="h1"><?= $page->title()->html() ?></h1>
+      <div class="text">
+        <?= $page->text()->kt() ?>
+      </div>
+    </article>
+</div>
+</section>
+
 
 <?php snippet('footer') ?>
--- www/site/templates/projects.php
+++ www/site/templates/projects.php
@@ -9,12 +9,14 @@
           <li>
             
           <div class="project-list__image [ measure-short ]">
-            
-            <img src="<?= $project->image()->crop(560, 380)->url() ?>" alt="<?= $project->title() ?> image">
+          <?php if ($image = $project->cover()): ?>
+            <?php # would need to be if ($image = $project->cover()->toFile()): but i have a model that does this for me ?>
+            <img src="<?= $image->crop(560, 380)->url() ?>" alt="<?= $project->title() ?>">
+          <?php endif ?>
           </div>
               <div class="project-list__info">
                 <h3 class="[ flow-space-300 ]"><?= $project->title() ?></h3>
-                <?php if ($project->text()->isNotEmpty()): ?><p class="[ flow-space-200 ]"><?= $project->text()->excerpt(30) ?></p><?php endif ?>
+                <?php if ($project->text()->isNotEmpty()): ?><p class="[ flow-space-200 ]"><?= $project->text()->excerpt(280) ?></p><?php endif ?>
                 <a class="breakout-button" href="<?= $project->url() ?>"></a>
               </div>
             
